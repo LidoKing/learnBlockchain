3d9c24ce11ac2e2d17a8ddd5181f13a9ba230536
LidoKing <kinglido203@gmail.com>
2021-05-31 21:11:06 +0800

    use of %x and other verbs for printing from fmt package

--- play.go
+++ play.go
@@ -6,7 +6,65 @@ import (
   "bytes"
   "encoding/binary"
   "math/big"
+  "crypto/sha256"
 )
+
+/*-------------------------------basics-------------------------------*/
+
+func StringByte(msg string) []byte {
+  result := []byte(msg)
+  // ^ Convert string into byte array with each character corresponding to a number in ASCII (Dec)
+  // Reference: http://3.bp.blogspot.com/-OCnzi-TOfKQ/VFnGx4IWnKI/AAAAAAAADIA/IlM6qC-VEWg/s1600/ascii%2Bto%2Bdec.png
+
+  return result
+}
+
+func WhatIsSliceOfByte() []byte {
+  // []byte{}: an array of numbers less than 1 byte (i.e. >=255)
+
+  result := [][]byte{ToHex(2), ToHex(3)}
+  // ^ [[0 0 0 0 0 0 0 2] [0 0 0 0 0 0 0 3]] (byte in byte)
+
+  joint := bytes.Join(result, []byte{})
+  // Combines two dimensional slice of byte to one slice of byte
+  // ^ [0 0 0 0 0 0 0 2 0 0 0 0 0 0 0 3]
+
+  return joint
+}
+
+/*-------------------------------block.go-------------------------------*/
+
+type Block struct {
+  Hash     []byte
+  Data     []byte
+  PrevHash []byte
+}
+
+func HashedByte(msg string) {
+  hash := sha256.Sum256(StringByte(msg))
+  final := hash[:]
+	fmt.Printf("%x\n" ,final)
+  // ^ %x	base 16, lower-case, two characters per byte (GO Documentation)
+}
+
+
+func (b *Block) DeriveHash() { // (b *Block) 'gets instance' of block struct to access the fields
+  info := bytes.Join([][]byte{b.Data, b.PrevHash}, []byte{})
+  // ^ This will join our previous block's relevant info with the new blocks
+  hash := sha256.Sum256(info)
+  // ^ This performs the actual hashing algorithm
+  b.Hash = hash[:]
+}
+
+func CreateBlock(data string, prevHash []byte) *Block {
+  block := &Block{[]byte{}, []byte(data), prevHash}
+  // ^ Create block given current data and hash of previous block
+  block.DeriveHash()
+  // ^ Generate hash of current block
+  return block
+}
+
+/*-------------------------------proof.go-------------------------------*/
 
 func WhatIsLeftShift() {
   target := big.NewInt(10)
@@ -14,7 +72,7 @@ func WhatIsLeftShift() {
 
   target.Lsh(target, uint(2))
   // ^ Lsh() (i.e. 'target' times 2 by (256-Difficulty) times)
-  // 3: [0 0 1 1] --(left shift all 1 by 2 bit)>> 12: [1 1 0 0]
+  // 3: [0 0 1 1] --(left shift all 1 by 2 bits)>> 12: [1 1 0 0]
   fmt.Println("new result:", target)
 }
 
@@ -42,19 +100,8 @@ func InitNonce(nonce int) []byte {
   return data
 }
 
-func WhatIsSliceOfByte() []byte {
-  // []byte{} == []
-
-  result := [][]byte{ToHex(2), ToHex(3)}
-  // ^ [[0 0 0 0 0 0 0 2] [0 0 0 0 0 0 0 3]] (byte in byte)
-
-  joint := bytes.Join(result, []byte{})
-  // Combines two dimensional slice of byte to one slice of byte
-  // ^ [0 0 0 0 0 0 0 2 0 0 0 0 0 0 0 3]
-
-  return joint
-}
+/*-------------------------------main-------------------------------*/
 
 func main() {
-  WhatIsLeftShift()
+  HashedByte("hello world")
 }
