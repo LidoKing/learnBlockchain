2aac8d35f3c1149750da6d746e79ccbc4f45fe45
LidoKing <kinglido203@gmail.com>
2021-06-01 22:24:39 +0800

    implemented actual 'mining' function

--- blockchain/proof.go
+++ blockchain/proof.go
@@ -28,7 +28,7 @@ func NewProof(b *Block) *ProofOfWork {
   return pow
 }
 
-// Turn an int into slice of byte with the greatest number for one index being 255 (i.e. 255 = [0 0 0 0 0 0 0 255] -> 256 = [0 0 0 0 0 0 1 0])
+// Convert int into slice of byte with the greatest number for one index being 255 (i.e. 255 = [0 0 0 0 0 0 0 255] -> 256 = [0 0 0 0 0 0 1 0])
 func ToHex(num int64) []byte {
   buff := new(bytes.Buffer)
   err := binary.Write(buff, binary.BigEndian, num)
@@ -36,4 +36,43 @@ func ToHex(num int64) []byte {
     log.Panic(err)
   }
   return buff.Bytes()
+}
+
+func (pow *ProofOfWork) InitNonce(nonce int) []byte {
+  data := bytes.Join(
+    [][]byte{
+      pow.Block.PrevHash,
+      pow.Block.Data,
+      ToHex(int64(nonce)),
+      ToHex(int64(Difficulty)),
+    },
+    []byte{},
+  )
+
+  return data
+}
+
+func (pow *ProofOfWork) Run() (int, []byte) {
+  var intHash big.Int
+  var hash [32]byte
+
+  nonce := 0
+    // This is essentially an infinite loop due to how large
+    // MaxInt64 is.
+  for nonce < math.MaxInt64 {
+    data := pow.InitNonce(nonce)
+    hash = sha256.Sum256(data)
+
+    fmt.Printf("\r%x", hash)
+    intHash.SetBytes(hash[:])
+
+    if intHash.Cmp(pow.Target) == -1 {
+      break
+    } else {
+      nonce++
+    }
+  }
+  fmt.Println()
+
+  return nonce, hash[:]
 }
