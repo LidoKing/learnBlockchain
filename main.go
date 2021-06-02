c5a730711237019574a20527490b7ceffa6829df
LidoKing <kinglido203@gmail.com>
2021-06-02 16:50:09 +0800

    added logic for hash validation

--- main.go
+++ main.go
@@ -2,6 +2,7 @@ package main
 
 import (
   "fmt"
+  "strconv"
   "github.com/LidoKing/learnBlockchain/blockchain"
 )
 
@@ -13,8 +14,13 @@ func main() {
   chain.AddBlock("third block after genesis")
 
   for _, block := range chain.Blocks {
+    pow := blockchain.NewProofOfWork(block)
+    fmt.Printf("Pow: %s\n", strconv.FormatBool(pow.Validate()))
+    fmt.Println()
+
     fmt.Printf("Previous hash: %x\n", block.PrevHash)
     fmt.Printf("data: %s\n", block.Data)
     fmt.Printf("hash: %x\n", block.Hash)
+    fmt.Printf("nonce: %d\n", block.Nonce)
   }
 }
