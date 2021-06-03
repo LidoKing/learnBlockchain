9b15aaab5acc40a762bf6dbebed096202b9f0bcb
LidoKing <kinglido203@gmail.com>
2021-06-03 12:38:02 +0800

    added functions for byte-block conversions

--- blockchain/block.go
+++ blockchain/block.go
@@ -1,14 +1,49 @@
-package blockchain
 
+
+import (
+  "log"
+  "bytes"
+  "encoding/gob"
+)
+
+/*---------------------------utils---------------------------*/
+func Handle(err error) {
+  if err != nil {
+    log.Panic(err)
+  }
+}
+
+// Blocks to bytes
+func (b *Block) Serialize() []byte {
+  var res bytes.Buffer
+  encoder := gob.NewEncoder(&res)
+
+  err := encoder.Encode(b)
+
+  Handle(err)
+
+  return res.Bytes()
+}
+
+// Bytes to blocks
+func Deserialize(data []byte) *Block {
+  var block Block
+
+  decoder := gob.NewDecoder(bytes.NewReader(data))
+
+  err := decoder.Decode(&block)
+
+  Handle(err)
+
+  return &block
+}
+
+/*---------------------------block---------------------------*/
 type Block struct {
   Hash     []byte
   Data     []byte
   PrevHash []byte
   Nonce    int
-}
-
-type BlockChain struct{
-  Blocks []*Block
 }
 
 func CreateBlock(data string, prevHash []byte) *Block {
@@ -24,20 +59,7 @@ func CreateBlock(data string, prevHash []byte) *Block {
   return block
 }
 
-func (chain *BlockChain) AddBlock(data string) {
-  prevBlock := chain.Blocks[len(chain.Blocks)-1] // 'len(chain.blocks)-1 gets the index of current latest block (i.e. previous block)'
-  // ^ Get old block to get hash to create new block
-  new := CreateBlock(data, prevBlock.Hash)
-  // ^ Create new block
-  chain.Blocks = append(chain.Blocks, new)
-  // ^ Append new block to chain
-}
-
 func Genesis() *Block {
   return CreateBlock("Genesis", []byte{})
   // ^ Create genesis block
-}
-
-func InitBlockChain() *BlockChain {
-  return &BlockChain{[]*Block{Genesis()}}
 }
