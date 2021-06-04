3f68ce63a94cac7ff6fe81d91d7b6071b94abb94
LidoKing <kinglido203@gmail.com>
2021-06-04 15:08:17 +0800

    added cli support

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -8,19 +8,49 @@ import (
 const dbPath = "./tmp/blocks"
 
 type BlockChain struct{
-  LastHash []*Block
+  LastHash []byte
   Database *badger.DB
 }
 
+// Get chain from database for printing info
+type BlockChainIterator struct {
+  CurrentHash []byte
+  Database    *badger.DB
+}
+
 func (chain *BlockChain) AddBlock(data string) {
-  prevBlock := chain.Blocks[len(chain.Blocks)-1] // 'len(chain.blocks)-1 gets the index of current latest block (i.e. previous block)'
-  // ^ Get old block to get hash to create new block
+  var lastHash []byte
 
-  new := CreateBlock(data, prevBlock.Hash)
-  // ^ Create new block
+  // Get lastHash from database
+  err := chain.Database.View(func(txn *badger.Txn) error { // error 1
+    item, err := txn.Get([]byte("lh")) // error 2
+    Handle(err) // Handle error 2
 
-  chain.Blocks = append(chain.Blocks, new)
-  // ^ Append new block to chain
+    err = item.Value(func(val []byte) error { // error 1
+      lastHash = val
+      return nil
+    })
+    Handle(err) // Handle error 1
+
+    return err // error 1
+  })
+  Handle(err) // Handle error 1
+
+  // Create new block with hash retrieved from database
+  newBlock := CreateBlock(data, lastHash)
+
+  // Add new block to database and update lastHash
+  err = chain.Database.Update(func(txn *badger.Txn) error { // error 3
+    err := txn.Set(newBlock.Hash, newBlock.Serialize()) // error 4
+    Handle(err) // Handle error 4
+
+    err = txn.Set([]byte("lh"), newBlock.Hash) // error 3
+
+    chain.LastHash = newBlock.Hash
+
+    return err // error 3
+  })
+  Handle(err) // Handle error 3
 }
 
 func InitBlockChain() *BlockChain {
@@ -33,12 +63,17 @@ func InitBlockChain() *BlockChain {
   err = db.Update(func(txn *badger.Txn) error { //error 4
 
     // Create chain with Genesis()
-    if _, err :=txn.Get([]byte("lh")); err == badger.ErrKeyNotFound { // "lh" name of key, no key also means no chain
+    // "lh" is name of key, no last hash also means no blocks created yet
+    if _, err := txn.Get([]byte("lh")); err == badger.ErrKeyNotFound {
       fmt.Println("No existing blockchain found")
       genesis := Genesis()
       fmt.Println("Genesis proved")
-      err = txn.Set(genesis.Hash, genesis.Serialize()) // genesis.Hash as key, serialized as value // error 2
+
+      // Add block to database
+      err = txn.Set(genesis.Hash, genesis.Serialize()) // error 2
       Handle(err) // Handle error 2
+
+      // Update last hash in database
       err = txn.Set([]byte("lh"), genesis.Hash) // error 4.1
 
       lastHash = genesis.Hash
@@ -50,7 +85,11 @@ func InitBlockChain() *BlockChain {
       item, err := txn.Get([]byte("lh")) // error 3
       Handle(err) //Handle error 3
 
-      lastHash, err = item.Value() // error 4.2
+      err = item.Value(func(val []byte) error { // error 4.2
+        lastHash = val
+        return nil
+      })
+      Handle(err) // Handle error 4.2
 
       return err // error 4.2
     }
@@ -58,6 +97,36 @@ func InitBlockChain() *BlockChain {
 
   Handle(err) // Handle error 4
 
-  blockchain := Blockchain{lastHash, db}
+  blockchain := BlockChain{lastHash, db}
   return &blockchain
+}
+
+func (chain *BlockChain) Iterator() *BlockChainIterator {
+  iter := &BlockChainIterator{chain.LastHash, chain.Database}
+
+  return iter
+}
+
+func (iter *BlockChainIterator) Next() *Block {
+  var block *Block
+
+  err := iter.Database.View(func(txn *badger.Txn) error { // error 1
+    item, err := txn.Get(iter.CurrentHash) // error 2
+    Handle(err) // Handle error 2
+
+    // Get current block with current hash and convert it from byte to block struct
+    err = item.Value(func(val []byte) error { // error 1
+      block = Deserialize(val)
+      return nil
+    })
+    Handle(err) // Handle error 1
+
+    return err // error 1
+  })
+  Handle(err) // Handle error 1
+
+  // Set current hash of iterator to hash of previous block for next iteration
+  iter.CurrentHash = block.PrevHash
+
+  return block
 }
--- main.go
+++ main.go
@@ -3,24 +3,100 @@ package main
 import (
   "fmt"
   "strconv"
+  "runtime"
+  "flag"
+  "os"
   "github.com/LidoKing/learnBlockchain/blockchain"
 )
 
-func main() {
-  chain := blockchain.InitBlockChain()
+type CommandLine struct {
+  blockchain *blockchain.BlockChain
+}
 
-  chain.AddBlock("first block after genesis")
-  chain.AddBlock("second block after genesis")
-  chain.AddBlock("third block after genesis")
+func (cli *CommandLine) printUsage() {
+  fmt.Println("Usage:")
+  fmt.Println(" add -block BLOCK_DATA -> Add a block to the chain")
+  fmt.Println(" print -> Prints the blocks in the chain")
+}
 
-  for _, block := range chain.Blocks {
+func (cli *CommandLine) validateArgs() {
+  // Check command line arguments in the form of array (e.g. >> a b c d, len(os.Args) = 4)
+  if len(os.Args) < 2 {
+    cli.printUsage()
+
+    // Exit application by shutting down GO routine
+    runtime.Goexit()
+  }
+}
+
+func (cli *CommandLine) addBlock(data string) {
+  cli.blockchain.AddBlock(data)
+  fmt.Println("Added Block!")
+}
+
+func (cli *CommandLine) printChain() {
+  iter := cli.blockchain.Iterator()
+
+  for {
+    block := iter.Next()
+
+    // Block info
     fmt.Printf("Previous hash: %x\n", block.PrevHash)
     fmt.Printf("data: %s\n", block.Data)
     fmt.Printf("hash: %x\n", block.Hash)
     fmt.Printf("nonce: %d\n", block.Nonce)
 
+    // PoW validation
     pow := blockchain.NewProofOfWork(block)
     fmt.Printf("Pow: %s\n", strconv.FormatBool(pow.Validate()))
     fmt.Println()
+
+    // Genesis block has no previous hash and therefore loop breaks
+    if len(block.PrevHash) == 0 {
+      break
+    }
   }
+}
+
+func (cli *CommandLine) run() {
+  cli.validateArgs()
+
+  addBlockCmd := flag.NewFlagSet("add", flag.ExitOnError)
+  printChainCmd := flag.NewFlagSet("print", flag.ExitOnError)
+  addBlockData := addBlockCmd.String("block", "", "Block data")
+
+  switch os.Args[1] {
+  case "add":
+    err := addBlockCmd.Parse(os.Args[2:])
+    blockchain.Handle(err)
+
+  case "print":
+    err := printChainCmd.Parse(os.Args[2:])
+    blockchain.Handle(err)
+
+  default:
+    cli.printUsage()
+    runtime.Goexit()
+  }
+
+  if addBlockCmd.Parsed() {
+    if *addBlockData == "" {
+      addBlockCmd.Usage()
+      runtime.Goexit()
+    }
+    cli.addBlock(*addBlockData)
+  }
+
+  if printChainCmd.Parsed() {
+    cli.printChain()
+  }
+}
+
+func main() {
+  defer os.Exit(0)
+  chain := blockchain.InitBlockChain()
+  defer chain.Database.Close()
+
+  cli := CommandLine{chain}
+  cli.run()
 }
