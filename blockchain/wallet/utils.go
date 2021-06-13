0a2da7ac880f2dec63662cfde9b0b255e8cd625e
LidoKing <kinglido203@gmail.com>
2021-06-13 13:49:38 +0800

    added functions for encoding and ecoding wallet addresses

---
+++ blockchain/wallet/utils.go
@@ -0,0 +1,25 @@
+package wallet
+
+import (
+  "log"
+
+  "github.com/mr-tron/base58"
+  "github.com/LidoKing/learnBlockchain/blockchain"
+)
+
+// base58 encode gives shorter output and prevents character identification issues
+// zero(0), capital O(O), capital i(I), lowercase L(l)
+func base58Encode(input []byte) []byte {
+  encode := base58.Encode(input)
+
+  // typecast string to slice of bytes
+  return []byte(encode)
+}
+
+func base58Decode(input []byte) []byte {
+  // typecast slice of bytes to string
+  decode, err := base58.Decode(string(input[:]))
+  Handle(err)
+
+  return decode
+}
