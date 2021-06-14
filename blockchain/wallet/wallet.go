c0076dcdda9a16e1c9088bf49ddaa52eace35b6d
LidoKing <kinglido203@gmail.com>
2021-06-14 22:30:39 +0800

    added remaining funcs for getting final address

--- blockchain/wallet/wallet.go
+++ blockchain/wallet/wallet.go
@@ -39,3 +39,42 @@ func MakeWallet() *Wallet {
 
   return &wallet
 }
+
+// Hash through sha256 and then ripemd160 to get public hash
+func PublicKeyHash(pubKey []byte) []byte {
+  pubHash :=sha256.Sum256(pubKey)
+
+  // Create hasher
+  hasher := ripemd160.New()
+  // Write pubKey into hasher
+  _, err := hasher.Write(pubHash[:])
+  Handle(err)
+
+  // Actual hashing
+  publicRipeMd := hasher.Sum(nil)
+  return publicRipeMd
+}
+
+func Checksum(ripeMdHash []byte) []byte {
+  firstHash := sha256.Sum256(ripeMdHash)
+  secondHash := sha256.Sum256(firstHash[:])
+
+  // Get first four bytes
+  return secondHash[:checksumLength]
+}
+
+func (w Wallet) Address() {
+   pubHash := PublicKeyHash(w.PublicKey)
+
+   vevrsionedHash := append([]byte{version}, pubHash...)
+   checksum := Checksum(versionedHash)
+
+   finalHash := append(versionedHash, checksum...)
+   address := Base58Encode(finalHash)
+
+   fmt.Printf("Public key: %x\n", w.PublicKey)
+   fmt.Printf("Public hash: %x\n", pubHash)
+   fmt.Printf("Address: %x\n", address)
+
+   return address
+}
