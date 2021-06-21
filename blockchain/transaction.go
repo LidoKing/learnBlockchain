1e541762f37d6ded9e57e1ea77061494ea79a7d8
LidoKing <kinglido203@gmail.com>
2021-06-21 23:53:56 +0800

    signing and verification

--- blockchain/transaction.go
+++ blockchain/transaction.go
@@ -7,6 +7,7 @@ import (
   "crypto/sha256"
   "encoding/gob"
   "fmt"
+  "strings"
 )
 
 const reward = 100
@@ -23,19 +24,27 @@ func (tx *Transaction) IsCoinbase() bool {
   return len(tx.Inputs) == 1 && len(tx.Inputs[0].ID) == 0 && tx.Inputs[0].Out == -1
 }
 
-/*--------------------------main---------------------------*/
-
-// Create hash based on transaction in the form of bytes
-func (tx *Transaction) SetID() {
+func (tx *Transaction) Serialize() []byte {
   var encoded bytes.Buffer
-  var hash [32]byte
-
-  encode := gob.NewEncoder(&encoded)
-  err := encode.Encode(tx)
+  enc := gob.NewEncoder(&encoded)
+  err := enc.Encode(tx)
   Handle(err)
 
-  hash = sha256.Sum256(encoded.Bytes())
-  tx.ID = hash[:]
+  return encoded.Bytes()
+}
+
+/*--------------------------main---------------------------*/
+
+// Convert transaction into bytes then hash it to get ID
+func (tx *Transaction) Hash() []byte {
+  var hash [32]byte
+
+  txCopy := *tx
+  txCopy.ID := []byte{}
+
+  hash := sha256.Sum256(txCopy.Serialize())
+
+  return hash[:]
 }
 
 func CoinbaseTx(toAddress string, data string) *Transaction {
@@ -91,3 +100,123 @@ func NewTransaction(from, to string, amount int, chain *BlockChain) *Transaction
 
   return &tx
 }
+
+func (tx *Transaction) TrimmedCopy() Transaction {
+  var inputs []TxInput
+  var outputs []TxOutput
+
+  for _, in := range tx.Inputs {
+    inputs = append(inputs, TxInput{in.Id, in.Out, nil, nil})
+  }
+
+  for _, out := range tx.Outputs {
+    outputs := append(outputs, TxPutput{out .Value, out.PubKeyHash})
+  }
+
+  txCopy := Transaction{tx.Id, inputs, outputs}
+
+  return txCopy
+}
+
+func (tx *Transaction) Sign(privateKey ecdsa.PrivateKey, prevTXs map[string]Transaction) {
+  if tx.IsCoinbase() {
+    return
+  }
+
+  for _, in := range tx.Inputs {
+    if prevTXs[hex.EncodeToString(in.ID)].ID == nil {
+      log.Panic("ERROR: Previous transaction does not exist")
+    }
+  }
+
+  // Create copy to get sig without modifying actual tx
+  txCopy := tx.TrimmedCopy()
+
+  for inId, in := range txCopy.Inputs {
+    // Get transaction where the input points to
+    prevTx := prevTXs[hex.EncodeToString(in.ID)]
+    txCopy.Inputs[inId].Signature = nil
+    // Set PubKey field for signing
+    txCopy.Inputs[inId].PubKey = prevTX.Outputs[in.Out].PubKeyHash
+    txCopy.ID = txCopy.Hash()
+    // Clear PubKey field for transaction verififcation afterwards
+    txCopy.Inupts[inId].PubKey = nil
+
+    r, s, err := ecdsa.Sign(rand.Reader, &privateKey, txCopy.ID)
+    Handle(err)
+    signature := append(r.Bytes(), s.Bytes())
+
+    tx.Inputs[inId].Signature = signature
+  }
+}
+
+func (tx *Transaction) Verify(prevTXs map[string]Transaction) bool {
+  if tx.IsCoinbase() {
+    return true
+  }
+
+  for _, in := range tx.Inputs {
+    if prevTXs[hex.EncodeToString(in.ID)].ID == nil {
+      log.Panic("ERROR: Previous transaction does not exist")
+    }
+  }
+
+  txCopy := tx.TrimmedCopy()
+  curve := elliptic.P256()
+
+  for inId, in := range tx.Inputs {
+    prevTx := prevTXs[hex.EncodeToString(in.ID)]
+		txCopy.Inputs[inId].Signature = nil
+		txCopy.Inputs[inId].PubKey = prevTx.Outputs[in.Out].PubKeyHash
+		txCopy.ID = txCopy.Hash()
+	  txCopy.Inputs[inId].PubKey = nil
+
+    r := big.Int{}
+    s:= big.Int{}
+
+    sigLen := len(in.Signature)
+    r.SetBytes(in.Signature[:(sigLen / 2)])
+    s.SetBytes(in.Signature[(sigLen / 2):])
+
+    x := big.Int{}
+    y := big.Int{}
+    keyLen := len(in.PubKey)
+    x.SetBytes(in.PubKey[:(keyLen / 2)])
+		y.SetBytes(in.PubKey[(keyLen / 2):])
+  }
+}
+
+func (tx *Transaction) String() string {
+  var lines []string
+
+  lines = append(lines, fmt.Sprintf("--- Transaction %x:", tx.ID))
+  for i, input := range tx.Inputs {
+    lines = append(lines, fmt.Sprintf("     Input %d:", i))
+		lines = append(lines, fmt.Sprintf("       TXID:     %x", input.ID))
+		lines = append(lines, fmt.Sprintf("       Out:       %d", input.Out))
+		lines = append(lines, fmt.Sprintf("       Signature: %x", input.Signature))
+		lines = append(lines, fmt.Sprintf("       PubKey:    %x", input.PubKey))
+  }
+
+  for i, output := range tx.Outputs {
+    lines = append(lines, fmt.Sprintf("     Output %d:", i))
+		lines = append(lines, fmt.Sprintf("       Value:  %d", output.Value))
+		lines = append(lines, fmt.Sprintf("       Script: %x", output.PubKeyHash))
+  }
+
+  return strings.Join(lines, "\n")
+}
+
+/*
+// Create hash based on transaction in the form of bytes
+func (tx *Transaction) SetID() {
+  var encoded bytes.Buffer
+  var hash [32]byte
+
+  encode := gob.NewEncoder(&encoded)
+  err := encode.Encode(tx)
+  Handle(err)
+
+  hash = sha256.Sum256(encoded.Bytes())
+  tx.ID = hash[:]
+}*/
