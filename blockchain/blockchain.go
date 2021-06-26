34f80c0c23c208f64cfabc46601550bafe6cb0fc
LidoKing <kinglido203@gmail.com>
2021-06-26 23:32:52 +0800

    modified FindUTXO() for UTXO persistence implementation

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -230,18 +230,48 @@ func (chain *BlockChain) FindUnspentTransactions(pubKeyHash []byte) []Transactio
 }
 
 // From transactions that contains unspent outputs to unspent ouputs
-func (chain *BlockChain) FindUTXO(pubKeyHash []byte) []TxOutput {
-  var UTXOs []TxOutput
-  unspentTransactions := chain.FindUnspentTransactions(pubKeyHash)
+func (chain *BlockChain) FindUTXO() map[string]TxOutputs {
+  UTXO := make(map[string]TxOutputs)
+  spentTXOs := make(map[string][]int)
 
-  for _, tx := range unspentTransactions {
-    for _, out := range tx.Outputs {
-      if out.IsLockedWithKey(pubKeyHash) {
-        UTXOs = append(UTXOs, out)
+  iter := chain.Iterator()
+
+  for {
+    block := iter.Next()
+
+    for _, tx := range block.Transactions {
+      txID := hex.EncodeToString(tx.ID)
+
+      if tx.IsCoinbase() == false {
+        for _, in := range tx.Inputs {
+          inTxID := hex.EncodeToString(in.ID)
+          spentTXOs[inTxID] = append(spentTXOs[inTxID], in.Out)
+        }
+      }
+
+      Outputs:
+      for outIdx, out := range tx.Outputs {
+        if spentTXOs[txID] != nil {
+          for _, spentOut := range spentTXOs[txID] {
+            if spentOut == outIdx {
+              continue Outputs
+            }
+          }
+        }
+        // Get map
+        outs := UTXO[txID]
+        // Modify map
+        outs.Outputs = append(outs.Output, out)
+        // Set map
+        UTXO[txID] = outs
       }
     }
+
+    if len(block.PrevHash) == 0 {
+      break
+    }
   }
-  return UTXOs
+  return UTXO
 }
 
 func (chain *BlockChain) FindSpendableOutputs(pubKeyHash []byte, sendAmount int) (int, map[string][]int) {
