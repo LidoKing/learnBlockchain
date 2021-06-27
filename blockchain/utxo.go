81d6da276b9c6f2e450a2cfa8ba0056cf4511c3b
LidoKing <kinglido203@gmail.com>
2021-06-27 22:35:11 +0800

    rewrote funcs from blockchain.go to read UTXOs from persistence layer

--- blockchain/utxo.go
+++ blockchain/utxo.go
@@ -1,5 +1,10 @@
 package blockchain
 
+import (
+  "bytes"
+  "log"
+)
+
 var (
   utxoPrefix = []byte("utxo-")
   prefixLength = len(utxoPrefix)
@@ -10,24 +15,85 @@ type UTXOSet struct {
   Blockchain *BlockChain
 }
 
-func (u *UTXOSet) Reindex() {
+// Find all UTXOs owned by specific address (pubKeyHash)
+// Replaced iterating from scratch with just checking if UTXOs can be unlocked
+func (u UTXOSet) FindUTXO(pubKeyHash []byte) []TxOutput {
+  var UTXOs []TxOutput
   db := u.Blockchain.Database
 
-  u.DeleteByPrefix(utxoPrefix)
+  err := db.View(func(txn *badger.Txn) error {
+    it := txn.NewIterator(badger.DefaultIteratorOptions)
+    defer it.Close()
 
-  UTXO := u.Blockchain.FindUTXO()
+    for it.Seek(utxoPrefix); it.ValidForPrefix(utxoPrefix); it.Next() {
+      item := it.Item()
+      v, err := item.Value()
+      Handle(err)
+      outs := DeserializeOutputs(v)
 
-  err := db.Update(func(txn *badger.Txn) error {
-    for txId, outs := range UTXO {
-      key, err := hex.DecodeString(txId)
-      if err != nil {
-        return err
+      for _, out := range outs.Outputs {
+        if out.IsLockedWithKey(pubKeyHash) {
+          UTXOs = append(UTXOs, out)
+        }
       }
-      key = append(utxoPrefix, key...)
-
-      err = txn.Set(key, outs.Serialize())
     }
+    return nil
   })
+  Handle(err)
+
+  return UTXOs
+}
+
+func (u UTXOSet) FindSpendableOutputs(pubKeyHash []byte, sendAmount int) (int, map[string][]int) {
+  unspentOuts := make(map[string][]int)
+  accumulated := 0
+  db := u.Blockchain.Database
+
+  err := db.View(func(txn *badger.Txn) error {
+    it := txn.NewIterator(badger.DefaultIteratorOptions)
+    defer it.Close()
+
+    for it.Seek(utxoPrefix); it.ValidForPrefix(utxoPrefix); it.Next() {
+      item := it.Item()
+      k := item.Key()
+      v, err := item.Value()
+      Handle(err)
+      k = bytes.TrimPrefix(k, utxoPrefix)
+      txID := hex.EncodeToString(k)
+      outs := DeserializeOutputs(v)
+
+      for outIdx, out := range outs.Ouputs {
+        if out.IsLockedWithKey(pubKeyHash) && accumulated < sendAmount {
+          accumulated += out.Value
+          unspentOuts[txID] = append(unspentOuts[txID], outIdx)
+        }
+      }
+    }
+    return nil
+  })
+  Handle(err)
+
+  return accumulated, unspentOuts
+}
+
+// Count txs with unspent outputs
+func (u UTXOSet) CountTransactions() int {
+  db := u.Blockchain.Database
+  counter := 0
+
+  err := db.View(func(txn *badger.Txn) error {
+    it := txn.NewIterator(badger.DefaultIteratorOptions)
+    defer it.Close()
+
+    for it.Seek(utxoPrefix); it.ValidForPrefix(utxoPrefix); it.Next() {
+      counter++
+    }
+
+    return nil
+  })
+  Handle(err)
+
+  return counter
 }
 
 func (u *UTXOSet) DeleteByPrefix(prefix []byte) {
@@ -58,7 +124,7 @@ func (u *UTXOSet) DeleteByPrefix(prefix []byte) {
     keysForDelete := make([][]byte, 0, collectSize)
     keysCollected := 0
 
-    for it.Seek(prefix); it.ValidForPrefix(prefix); it.Next() {
+    for it.Seek(utxoPrefix); it.ValidForPrefix(utxoPrefix); it.Next() {
       item := it.Item()
       key := item.KeyCopy(nil)
       keysForDelete = append(keysForDelete, key)
@@ -79,5 +145,90 @@ func (u *UTXOSet) DeleteByPrefix(prefix []byte) {
        }
     }
     return nil
+  })
+}
+
+// Match with blocchain updates
+// Prevent conflict when comparing indices in Update()
+func (u *UTXOSet) Reindex() {
+  db := u.Blockchain.Database
+
+  // Clear out database 'utxo-' prefix
+  // Rebuild set in the following
+  u.DeleteByPrefix(utxoPrefix)
+
+  UTXO := u.Blockchain.FindUTXO()
+
+  // Get key of map and add prefix
+  err := db.Update(func(txn *badger.Txn) error {
+    for txId, outs := range UTXO {
+      key, err := hex.DecodeString(txId)
+      if err != nil {
+        return err
+      }
+      key = append(utxoPrefix, key...)
+
+      // Use 'prefixed key' as key in database
+      // Key: txID with prefix, Value: serialized UTXOs in the tx
+      err = txn.Set(key, outs.Serialize())
+      Handle(err)
+    }
+  })
+}
+
+// Remove outsputs that are originally unspent but are now referenced and used
+// Add newest unspent outputs if there are any
+func (u *UTXOSet) Update(block *Block) {
+  db := u.Blockchain.Database
+
+  err := db.Update(func(txn *badger.Txn) error {
+    for _, tx := range block.Transactions {
+      if tx.ISCoinbase() == false {
+        for _, in := range tx.Inputs {
+          updatedOuts := TxOutputs{}
+          inID := append(utxoPrefix, in.ID...) // i.e. prefixed ID of transaction referenced by input
+          item, err := txn.Get(inID)
+          Handle(err)
+          // v: serialized TxOutputs struct which contains UTXOs of tx referenced by the input
+          v, err := item.Value()
+          Handle(err)
+
+          outs := DeserializeOutputs(v)
+
+          for outIdx, out := range outs.Outputs {
+            // Potential conflict solved by reindexing
+            if outIdx != in.Out {
+              // Add ouput to updatedOuts if it remains unspent after new transaction
+              updatedOuts.Outputs = append (updatedOuts.Outputs, out)
+            }
+          }
+
+          // Delete key if original UTXOs are spent with no more UTXOs in the tx
+          // (i.e value of the item has nothing)
+          if len(updatedOuts.Outputs) == 0 {
+            if err := txn.Delete(inID); err != nil {
+              log.Panic(err)
+            }
+          } else {
+            // Update the item otherwise
+            if err := txn.Set(inID, updatedOuts.Serialize()); err != nil {
+              log.Panic(err)
+            }
+          }
+        }
+      }
+      // Logic for coinbase tx
+      newOutputs:= TxOutputs{}
+      // Output must be unspent for coinbase tx
+      // No checking is needed
+      for _, out := range tx.Outputs {
+        newOutputs.Outputs = append(newOutputs.Outputs, out)
+      }
+
+      txID := append(utxoPrefix, tx.ID...)
+      if err := txn.Set(txID, newOutputs.Outputs.Serialize()); err != nil {
+        log.Panic(err)
+      }
+    }
   })
 }
