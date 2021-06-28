8016d6a7519665581b9759ec161dfa77537b6d35
LidoKing <kinglido203@gmail.com>
2021-06-28 21:17:32 +0800

    added logic so that coinbase tx also returns true when verified

--- blockchain/blockchain.go
+++ blockchain/blockchain.go
@@ -260,6 +260,10 @@ func (bc *BlockChain) SignTransaction(tx *Transaction, privateKey ecdsa.PrivateK
 }
 
 func (bc *BlockChain) VerifyTransaction(tx *Transaction) bool {
+  if tx.IsCoinbase() {
+    return
+  }
+
   prevTXs := make(map[string]Transaction)
 
   for _, in := range tx.Inputs {
