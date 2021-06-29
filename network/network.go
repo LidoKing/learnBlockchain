de92d2e34c8396b741312043c6e425c03e8acea7
LidoKing <kinglido203@gmail.com>
2021-06-29 20:38:24 +0800

    declared global variables and data structs

---
+++ network/network.go
@@ -0,0 +1,63 @@
+package network
+
+import (
+  "fmt"
+  "runtime"
+  "os"
+  "gopkg.in/vrecan/death.v3"
+  "github.com/LidoKing/learnBlockchain/blockchain"
+)
+
+const (
+  protocol = "tcp"
+  version = 1
+  commandLength = 12
+)
+
+var (
+  nodeAddress string
+  minerAddress string
+  KnownNodes = []string{"localhost:3000"}
+  blocksInTransit = [][]byte{}
+  // Store txs until there are enough for mining
+  memPool = make(map[string]blockchain.Transaction)
+)
+
+// Following structures help identify type of data to be sent back and forth
+
+type Addr struct {
+  AddrList []string
+}
+
+type Block struct {
+  AddrFrom string
+  Block []byte
+}
+
+type GetBlocks struct {
+  AddrFrom string
+}
+
+type GetData struct {
+  AddrFrom string
+  Tpe string
+  ID []byte
+}
+
+type Inv struct {
+  AddrFrom string
+  Type string
+  Items [][]byte
+}
+
+type Tx struct {
+  AddrFrom string
+  Transaction []byte
+}
+
+// Identify when a chain needs to be updated
+type Version struct {
+  Version int
+  BestHeight int
+  AddrFrom string
+}
