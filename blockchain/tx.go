0f9e865b6c4f170f3a5d8b61b4eaf64453dd9058
LidoKing <kinglido203@gmail.com>
2021-07-16 15:28:42 +0800

    deleted useless func UsesKey()

--- blockchain/tx.go
+++ blockchain/tx.go
@@ -23,38 +23,11 @@ type TxOutputs struct {
 type TxInput struct {
   // Points to transaction where output is in
   ID []byte
-
   // Index of output that input points to
   Out int
+
   Sig []byte
   PubKey []byte
-}
-
-func (in *TxInput) UsesKey(pubKeyHash []byte) bool {
-  lockingHash := wallet.PublicKeyHash(in.PubKey)
-
-  return bytes.Compare(lockingHash, pubKeyHash) == 0
-}
-
-// Lock output with address
-func (out *TxOutput) Lock(address []byte) {
-  pubKeyHash := wallet.Base58Decode(address)
-
-  // Remove version and checksum
-  pubKeyHash = pubKeyHash[1:len(pubKeyHash)-4]
-  out.PubKeyHash = pubKeyHash
-}
-
-// Check if unspent outputs belong to specific address
-func (out *TxOutput) IsLockedWithKey(pubKeyHash []byte) bool {
-  return bytes.Compare(out.PubKeyHash, pubKeyHash) == 0
-}
-
-func NewTXOutput(value int, address string) *TxOutput {
-  txo := &TxOutput{value, nil}
-  txo.Lock([]byte(address))
-
-  return txo
 }
 
 func (outs TxOutputs) Serialize() []byte {
@@ -71,4 +44,25 @@ func DeserializeOutputs(data []byte) TxOutputs {
   err := dec.Decode(&outputs)
   Handle(err)
   return outputs
+}
+
+// Check if unspent outputs belong to specific address
+func (out *TxOutput) IsLockedWithKey(pubKeyHash []byte) bool {
+  return bytes.Compare(out.PubKeyHash, pubKeyHash) == 0
+}
+
+// Lock output with address
+func (out *TxOutput) Lock(address []byte) {
+  pubKeyHash := wallet.Base58Decode(address)
+
+  // Remove version and checksum
+  pubKeyHash = pubKeyHash[1:len(pubKeyHash)-4]
+  out.PubKeyHash = pubKeyHash
+}
+
+func NewTXOutput(value int, address string) *TxOutput {
+  txo := &TxOutput{value, nil}
+  txo.Lock([]byte(address))
+
+  return txo
 }
