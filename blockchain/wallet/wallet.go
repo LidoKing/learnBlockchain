6b692fdd439750b6383319f2eafaa38d4959e334
LidoKing <kinglido203@gmail.com>
2021-07-16 15:43:27 +0800

    .

--- blockchain/wallet/wallet.go
+++ blockchain/wallet/wallet.go
@@ -45,7 +45,7 @@ func MakeWallet() *Wallet {
   return &wallet
 }
 
-// Hash through sha256 and then ripemd160 to get public hash
+// Hash through sha256 and then ripemd160 to get public key hash
 func PublicKeyHash(pubKey []byte) []byte {
   pubHash :=sha256.Sum256(pubKey)
 
@@ -69,9 +69,9 @@ func Checksum(versionedHash []byte) []byte {
 }
 
 func (w Wallet) Address() []byte {
-   pubHash := PublicKeyHash(w.PublicKey)
+   pubKeyHash := PublicKeyHash(w.PublicKey)
 
-   versionedHash := append([]byte{version}, pubHash...)
+   versionedHash := append([]byte{version}, pubKeyHash...)
    checksum := Checksum(versionedHash)
 
    fullHash := append(versionedHash, checksum...)
